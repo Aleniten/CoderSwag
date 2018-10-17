d37326cdd1619fc3a3f13c0f1cd68117a3fbe840
Jose Alejandro Herrero De Lorenzi <josealejandroherrerodelorenzi@MacBook-Air-de-Jose.local>
2018-10-17 14:33:44 -0300

    add own list view adpter

--- app/src/main/java/com/aleapp/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/aleapp/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aleapp.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.aleapp.coderswag.Adapters.CategoryAdapter
 import com.aleapp.coderswag.Model.Category
 import com.aleapp.coderswag.R
 import com.aleapp.coderswag.Services.DataServices
@@ -10,15 +11,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataServices.categories)
+        adapter = CategoryAdapter(this, DataServices.categories)
+
         categoryListView.adapter = adapter
     }
 }
