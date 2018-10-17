54cafccd250d224ac238630ed26296591e527ea6
Jose Alejandro Herrero De Lorenzi <josealejandroherrerodelorenzi@MacBook-Air-de-Jose.local>
2018-10-17 12:41:38 -0300

    lesson 1 and 2

---
+++ app/src/main/java/com/aleapp/coderswag/Controller/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.aleapp.coderswag.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.aleapp.coderswag.Model.Category
+import com.aleapp.coderswag.R
+import com.aleapp.coderswag.Services.DataServices
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataServices.categories)
+        categoryListView.adapter = adapter
+    }
+}
---
+++ app/src/main/java/com/aleapp/coderswag/Model/Product.kt
@@ -0,0 +1,4 @@
+package com.aleapp.coderswag.Model
+
+class Product (val title: String, val price: String, val image: String)
+
